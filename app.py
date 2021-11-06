2433a8f03ed259e6536d6798d1759feb4d46eed9
Frederick Chesneau <fchesneau@gmail.com>
2021-11-06 10:55:56 +0100

    sidebar

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.write("Hello!")
+m = st.markdown(""" <style> div.stButton > button:first-child { background-color: rgb(204, 49, 49); width: 100%; font-size: 32px} </style>""", unsafe_allow_html=True) 
+m2 = st.markdown(f'''
+    <style>
+    section[data-testid="stSidebar"] .css-ng1t4o {{width: 14rem;}}
+    </style>
+''',unsafe_allow_html=True)
+
+st.sidebar.title("TEAMS")
+st.sidebar.button("Herren")
+st.sidebar.button("Damen")
+st.sidebar.button("U18")
+
+
